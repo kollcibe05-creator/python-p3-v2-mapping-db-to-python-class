76f434750c4141e0bd66df8b5df45f3a2da61ae8
Collins Kibet <kollcibe05@gmail.com>
2025-11-28 15:36:17 +0300

    ORM row-to-object

--- lib/debug.py
+++ lib/debug.py
@@ -11,6 +11,12 @@ def reset_database():
     Department.create("Payroll", "Building A, 5th Floor")
     Department.create("Human Resources", "Building C, East Wing")
     Department.create("Accounting", "Building B, 1st Floor")
+    # workers = Department.create("Workers", "Ground")
+    # q_a = Department.create("Quality Assurance", "Top Floor")
+
+
+    # workers.delete()
+    # q_a.delete()
 
 reset_database()
 ipdb.set_trace()
